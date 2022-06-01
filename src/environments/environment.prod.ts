d724317b84345755cd65be0fd0c5bd77559cee22
Luca Caboni <99878411+luca-caboni@users.noreply.github.com>
2022-06-01 16:54:40 +0200

    fixed and ready to go

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  pathApi: '//epicode.online/epicodebeservice_v2',
+  myToken:
+    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY1MzYzNTE1MSwiZXhwIjoxOTY4OTk1MTUxfQ.TbqmVvOUiGSeqtvLAfKz82S3dNfVALe9VhoUZkH4QA6aDfdUGKOd0xN-JSnLxWbNpjFX5zMAxr4Rdz8inxS8qg',
+  tenantID: 'fe_0222b',
 };
